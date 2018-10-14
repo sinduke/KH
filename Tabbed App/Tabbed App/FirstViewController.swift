dae59461ca9b1fd9efe2ad2a1cbf3cabfa01160c
sinduke <sinduke@outlook.com>
2018-10-14 20:10:33 +0800

    sinduke Create a new Object

---
+++ Tabbed App/Tabbed App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tabbed App
+//
+//  Created by sinduke on 2018/10/14.
+//  Copyright © 2018年 sinduke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tabbed App/Tabbed App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbed App
+//
+//  Created by sinduke on 2018/10/14.
+//  Copyright © 2018年 sinduke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
